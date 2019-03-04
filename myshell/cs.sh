485e60497ad5a0d764800e451473b9fa8d47c04c
kpy <kuangpeiyu1@163.com>
2019-03-04 21:14:24 +0800

    change cs

--- myshell/cs.sh
+++ myshell/cs.sh
@@ -18,9 +18,11 @@ if [ ! -n "$1" ]; then
     cscope -Rbq
 else
     if [ ! -n "$2" ]; then
+        find $1 -name "*.h" -o -name "*.c" -o -name "*.cc" -o -name "*.cpp" >cscope.files
+    else
         find $1 -name "*.h" -o -name "*.c" -o -name "*.cc" -o -name "*.cpp" |egrep -v $2 >cscope.files
     fi
     cscope -Rbq -I cscope.files
 fi
 
-#cs "! -path './thirdparty'"
+#cs ./ "! -path './thirdparty'"
